a58089474a42e1226486d679737209795bc4a28d
Mark <feranmijogbodo@gmail.com>
2025-07-12 08:26:07 +0100

    new fixes for page

--- src/components/mobile-components/wrappers/mobile-services.tsx
+++ src/components/mobile-components/wrappers/mobile-services.tsx
@@ -1,11 +1,14 @@
+"use client";
 import bettingIcon from "@/assets/svg/bettingMobileIcon.svg";
 import airtimeIcon from "@/assets/svg/airtimeMobileIcon.svg";
 import giftCardIcon from "@/assets/svg/giftCardMobileIcon.svg";
 import otherIcon from "@/assets/svg/othersMobileIcon.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export const MobileDashboardService = () => {
+  const router = useRouter();
   return (
     <>
       <div className="my-3">
@@ -13,19 +16,34 @@ export const MobileDashboardService = () => {
           Other services
         </p>
         <div className="my-4 flex items-center justify-between">
-          <div className="flex flex-col items-center justify-center gap-2">
+          <div
+            onClick={() => {
+              router.push("/giftcard");
+            }}
+            className="flex flex-col cursor-pointer items-center justify-center gap-2"
+          >
             <Image src={giftCardIcon} alt="" />
             <p className="text-bayfi-black-500 font-grotesk-semi-bold text-sm">
               Gift card
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center gap-2">
+          <div
+            onClick={() => {
+              router.push("/buy-airtime");
+            }}
+            className="flex flex-col items-center justify-center gap-2"
+          >
             <Image src={airtimeIcon} alt="" />
             <p className="text-bayfi-black-500 font-grotesk-semi-bold text-sm">
               Airtime
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center gap-2">
+          <div
+            onClick={() => {
+              router.push("/betting");
+            }}
+            className="flex flex-col items-center justify-center gap-2"
+          >
             <Image src={bettingIcon} alt="" />
             <p className="text-bayfi-black-500 font-grotesk-semi-bold text-sm">
               Betting
