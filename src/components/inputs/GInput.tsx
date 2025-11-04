a3b8d20e8818fc0c246d73a010f7f4a700e78adc
Mark <feranmijogbodo@gmail.com>
2025-11-04 13:50:39 +0100

    new fix for overflowing input

--- src/components/inputs/GInput.tsx
+++ src/components/inputs/GInput.tsx
@@ -23,7 +23,7 @@ export const GInput = ({
   noMarginBottom?: boolean;
   setInput?: (val: string) => void;
   inputVal?: string;
-  disabled? : boolean
+  disabled?: boolean;
 }) => {
   const [showPassword, setShowPassword] = useState(false);
   return (
@@ -62,7 +62,7 @@ export const GInput = ({
           }}
           type={type === "password" ? (showPassword ? "text" : type) : type}
           placeholder={placeholder}
-          className={`bg-bayfi-grey-400 font-grotesk-medium placeholder:font-grotesk-regular text-base rounded-lg p-2 lg:py-3 ${icon && " px-9"}  min-w-full  outline-none border border-bayfi-grey-600 ${disabled && "cursor-not-allowed"}`}
+          className={`bg-bayfi-grey-400 font-grotesk-medium placeholder:font-grotesk-regular text-base rounded-lg p-2 lg:py-3 ${icon && " px-9"}  w-full outline-none border border-bayfi-grey-600 ${disabled && "cursor-not-allowed"}`}
         />
       </div>
     </div>
