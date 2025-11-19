de79231be81d3ff9e0d902f0cc2880cb99f297a8
Mark <feranmijogbodo@gmail.com>
2025-11-19 23:37:36 +0100

    new fixe

--- src/app/(landing-pages)/page.tsx
+++ src/app/(landing-pages)/page.tsx
@@ -7,7 +7,7 @@ import landingPhone from "@/assets/svg/landing-page-phone.svg";
 export default function Home() {
   return (
     <>
-      <section className="min-h-screen h-screen bg-[#1C1B1F] hero-bg flex justify-center items-center flex-col">
+      <section className="min-h-screen mb-[480px] h-screen bg-[#1C1B1F] hero-bg flex justify-center items-center flex-col">
         <Row className="w-full" justify={"center"}>
           <Col xs={12}>
             <div className="text-[64px] relative text-white text-center font-manrope-medium">
@@ -19,7 +19,7 @@ export default function Home() {
             <p className="text-[#EAEAF1] font-manrope-regular text-lg my-4 text-center">
               Siuuuuuper fast transactions
             </p>
-            <div className="flex items-center justify-center mt-12">
+            <div className="flex items-center justify-center mt-8">
               <Link href="/login">
                 <button className="bg-white rounded-full p-4 px-8 ">
                   <p className="font-manrope-semibold text-[#0C0C13]">
@@ -28,15 +28,20 @@ export default function Home() {
                 </button>
               </Link>
             </div>
-            <div className="flex items-center justify-center mt-4">
+            <div className="flex items-center justify-center ">
               <Image src={bitcoins} alt="" />
             </div>
             <div className="absolute w-full flex justify-center ">
-              <Image src={landingPhone} alt="" className="border-6 rounded-[48px] border-[#BEDD3A]" />
+              <Image
+                src={landingPhone}
+                alt=""
+                className="border-6 rounded-[48px] border-[#BEDD3A]"
+              />
             </div>
           </Col>
         </Row>
       </section>
+      <div className="text-black">Next content to be tested</div>
     </>
   );
 }
