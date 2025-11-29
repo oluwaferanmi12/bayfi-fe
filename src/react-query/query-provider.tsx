71c645fbd20ccdb99f3b812a4fac576ddb60ca74
Mark <feranmijogbodo@gmail.com>
2025-11-29 22:58:34 +0100

    new fixes

--- src/react-query/query-provider.tsx
+++ src/react-query/query-provider.tsx
@@ -5,7 +5,10 @@ import { ReactNode, useState } from "react";
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
   const [queryClient] = useState(
-    () => new QueryClient({ defaultOptions: { queries: { retry: 1 } } })
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { retry: 1, refetchOnWindowFocus: false } },
+      })
   );
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
