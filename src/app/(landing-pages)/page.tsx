1d0989df6eaabd3489388870ef9bd413c313ecf6
Mark <feranmijogbodo@gmail.com>
2025-11-30 12:00:33 +0100

    new fixes

--- src/app/(landing-pages)/page.tsx
+++ src/app/(landing-pages)/page.tsx
@@ -4,7 +4,13 @@ import { HeroSection } from "@/components/UIs/landing/hero-section";
 import { LifeEasySection } from "@/components/UIs/landing/life-easy-section";
 import { FeatureSection } from "@/components/UIs/landing/feature-section";
 import { HowItWorks } from "@/components/UIs/landing/how-it-works";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Home - Giftcard Trading Platform",
+  description:
+    "Welcome to our Giftcard Trading Platform, where you can easily buy, sell, and trade giftcards with confidence. Explore our user-friendly interface, secure transactions, and a wide variety of giftcards to choose from. Join our community of satisfied users and start trading today!",
+};
 export default function Home() {
   return (
     <>
