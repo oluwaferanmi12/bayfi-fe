d68b370ca1901df2688f70b3f3c02e93a81b3dba
Mark <feranmijogbodo@gmail.com>
2025-12-21 01:22:06 +0100

    new fix

--- src/components/mobile-components/wrappers/mobile-wrapper.tsx
+++ src/components/mobile-components/wrappers/mobile-wrapper.tsx
@@ -1,5 +1,14 @@
 import { ReactNode } from "react";
+import glowBg from "@/assets/svg/glow-bg.svg";
+import Image from "next/image";
 
 export const MobileWrapper = ({ children }: { children: ReactNode }) => {
-  return <div className="mobileWrapper lg:hidden p-4 bg-[#f6f4f0] ">{children}</div>;
+  return (
+    <div className=" lg:hidden p-4 bg-[#FAFAFA] relative">
+      <div className="fixed top-0 w-full h-[30vh] overflow-hidden left-0">
+        <Image src={glowBg} alt="" className="w-full" />
+      </div>
+      {children}
+    </div>
+  );
 };
